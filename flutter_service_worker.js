7dc92111eb57759ccd792d908b3c74149f45adf5
osanchez25 <osanchez25@hotmail.com>
2022-10-21 21:16:40 -0600

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ca8ce41e83eecb13ed96ef2fa4426d08",
-"/": "ca8ce41e83eecb13ed96ef2fa4426d08",
-"main.dart.js": "ad727b93fc40691c06051efc1560a3b2",
+"index.html": "529ad6c55f57c448650da8df28b14b15",
+"/": "529ad6c55f57c448650da8df28b14b15",
+"main.dart.js": "643e12ff87183c473df3ddc8755d609d",
 "manifest.json": "ee1c069aaf6dd480e5270a4f9f9c27b1",
 "version.json": "bf7da883f2770f6bd573786e9e4dfdf0"
 };
